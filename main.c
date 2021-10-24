079c779b616ee861624c88214851cb466f938eee
SollyBunny <sollybunn@gmail.com>
2021-10-24 11:32:35 +0000

    game works

--- main.c
+++ main.c
@@ -18,9 +18,16 @@
 
 #define KEY0 48
 
-#define GRIDSIZE 9 * 9 // shoudnt be changed
+#define GRIDSIZE (9 * 9) // shoudnt be changed
 
-unsigned int grid[GRIDSIZE];
+struct Tile {
+	unsigned int val;
+	unsigned int use;
+};
+
+struct Tile grid[GRIDSIZE];
+unsigned int tempgrid[GRIDSIZE];
+unsigned int tilesleft;
 
 char gridtxt[] = "                           \n╔═══════╦═══════╦═══════╗\n║ A A A ║ A A A ║ A A A ║\n║ A A A ║ A A A ║ A A A ║\n║ A A A ║ A A A ║ A A A ║\n╠═══════╬═══════╬═══════╣\n║ A A A ║ A A A ║ A A A ║\n║ A A A ║ A A A ║ A A A ║\n║ A A A ║ A A A ║ A A A ║\n╠═══════╬═══════╬═══════╣\n║ A A A ║ A A A ║ A A A ║\n║ A A A ║ A A A ║ A A A ║\n║ A A A ║ A A A ║ A A A ║\n╚═══════╩═══════╩═══════╝\n                           ";
 unsigned int gridpos[] = {28, 30, 32, 36, 38, 40, 44, 46, 48, 54, 56, 58, 62, 64, 66, 70, 72, 74, 80, 82, 84, 88, 90, 92, 96, 98, 100, 132, 134, 136, 140, 142, 144, 148, 150, 152, 158, 160, 162, 166, 168, 170, 174, 176, 178, 184, 186, 188, 192, 194, 196, 200, 202, 204, 236, 238, 240, 244, 246, 248, 252, 254, 256, 262, 264, 266, 270, 272, 274, 278, 280, 282, 288, 290, 292, 296, 298, 300, 304, 306, 308};
@@ -46,6 +53,13 @@ unsigned int cursorpos;
 	return 48 + i; // this really works lmao
 }*/
 
+// util function to get base 10 intiger "size"
+unsigned int intsize(unsigned int a) {
+	if      (a < 10       ) return 1;
+	else if (a < 100      ) return 2;
+	else                    return 3;
+}
+
 int checkgrid() {
 	//return 1;
 
@@ -62,12 +76,23 @@ int checkgrid() {
 	for (unsigned int i = 0; i < 9; ++i) {
 
 		for (unsigned int m = 0; m < 9; ++m) {
-			row[m] = grid[(i * 9) + m];
-			col[m] = grid[(m * 9) + i];
-			blk[m] = grid[((i * 3) % 9) + (i / 3 * 9 * 3) + (m % 3) + (m / 3 * 9)];
+			/*printf("row: %u, col: %u, blk: %u, row: %u, col: %u, blk: %u\n", 
+				(i * 9) + m, 
+				(m * 9) + i, 
+				((i * 3) % 9) + (i / 3 * 9 * 3) + (m % 3) + (m / 3 * 9),
+				grid[(i * 9) + m],
+				grid[(m * 9) + i],
+				grid[((i * 3) % 9) + (i / 3 * 9 * 3) + (m % 3) + (m / 3 * 9)]
+			);*/ // DEBUG
+			row[m] = grid[(i * 9) + m].val;
+			col[m] = grid[(m * 9) + i].val;
+			blk[m] = grid[((i * 3) % 9) + (i / 3 * 9 * 3) + (m % 3) + (m / 3 * 9)].val;
 
-
-
+			for (unsigned int _ = 0; _ < m; ++_) { // only have to compare upto whatve been set
+				if ((row[_] == row[m]) && (row[_] != 0)) return 0;
+				if ((col[_] == col[m]) && (col[_] != 0)) return 0;
+				if ((blk[_] == blk[m]) && (blk[_] != 0)) return 0;
+			}
 
 			// m += 9;
 			// j++;
@@ -174,7 +199,7 @@ void print() {
 	// i know this may be ineffecient but it makes the code so much more readable x-x
 
 	char * i;
-	unsigned int * m;
+	struct Tile * m;
 
 	/*unsigned int j = 0;
 	for (i = gridtxt; *i != '\n'; ++i) {
@@ -185,7 +210,7 @@ void print() {
 	m = grid;
 	for (i = gridtxt; *i; ++i) {
 		if (*i == 'A') {
-			putchar(digittochar(*m));
+			putchar(digittochar((*m).val));
 			++m;
 		} else if (*i == '\n') {
 			putchar(' ');
@@ -254,17 +279,15 @@ int main(void) {
 	tcsetattr(0, TCSANOW, &term);
 
 	printf("\x1b[2J\x1b[48;5;237m\x1b[?25l\x1b[H\x1b[1m\x1b[7m");
-
-	cursorpos = 0;
-
 	
 	for (unsigned int i = 0; i < GRIDSIZE; ++i) {
-		grid[i] = (
+		grid[i].val = (
 			i + 1 + // inital 1-9 pattern
-			(i / 9 * 4) + // offset 3 per row // TODO CHANGE TO 3
+			(i / 9 * 3) + // offset 3 per row
 			(i / (9 * 3)) // offset 9 per large row
-		) % 9 ;
-		if (grid[i] == 0) grid[i] = 9;
+		) % 9 + 1;
+		//grid[i].use = 0;
+		//if (grid[i].val == 0) grid[i].val = 9;
 	}
 
 	/*unsigned int i = 0;
@@ -276,53 +299,85 @@ int main(void) {
 	}*/
 
 	/*unsigned int temp = 0;
+	unsigned int r;
+
 	for (unsigned int i = 0; i < GRIDSIZE; ++i) {
 
 		temp = 0;
 		do {
 		
+			if (temp == 10) {
+				for (unsigned int _ = 0; _ < 10; _++) {
+					grid[rand() % GRIDSIZE + 1] = rand() % 9 + 1;
+				}
+				temp = 0;
+			};
 			temp++;
-			if (temp == 10) break;
-			grid[i] = temp;
+			r = rand() % 9 + 1;
+			grid[i] = r;
 			
-		} while (!checkgrid());
+		} while (0 == checkgrid());
 		
 	}*/
 
-	printf("THIS: %d\n", checkgrid());
-	
 	//board = [[(i + k) % 9 + 1 for i in range(1, height + 1)] for k in range(width)]
 	
-	/*unsigned int temp;
+	unsigned int temp;
 	unsigned int r1;
 	unsigned int r2;
 
 	for (unsigned int i = 0; i < 20; ++i) {
+		gridgenloopstart:
 
-		r1 = (rand() % 9) * 9;
-		r2 = (rand() % 9) * 9;
-		for (unsigned int m = 0; m < 9; ++m) {
-			temp = grid[r1 + m];
-			grid[r1 + m] = grid[r2 + m];
-			grid[r2 + m] = temp; 
+		for (unsigned int j = 0; j < GRIDSIZE; ++j) {
+			tempgrid[j] = grid[j].val;
+		}
+		if (rand() % 2 < 1) {
+			r1 = (rand() % 9) * 9;
+			r2 = (rand() % 9) * 9;
+			for (unsigned int m = 0; m < 9; ++m) {
+				temp = grid[r1 + m].val;
+				grid[r1 + m].val = grid[r2 + m].val;
+				grid[r2 + m].val = temp; 
+			}
+		} else {
+			r1 = (rand() % 9);
+			r2 = (rand() % 9);
+			for (unsigned int m = 0; m < 9; ++m) {
+				temp = grid[r1 + (m * 9)].val;
+				grid[r1 + (m * 9)].val = grid[r2 + (m * 9)].val;
+				grid[r2 + (m * 9)].val = temp; 
+			}
+		}
+
+		if (!checkgrid()) {
+			for (unsigned int j = 0; j < GRIDSIZE; ++j) {
+				grid[j].val = tempgrid[j];
+			}
+			goto gridgenloopstart;
 		}
 		
 	}
 
-	for (unsigned int i = 0; i < 20; ++i) {
-	
-		r1 = (rand() % 9);
-		r2 = (rand() % 9);
-		for (unsigned int m = 0; m < 9; ++m) {
-			temp = grid[r1 + (m * 9)];
-			grid[r1 + (m * 9)] = grid[r2 + (m * 9)];
-			grid[r2 + (m * 9)] = temp; 
-		}
-		
-	}*/
+	tilesleft = 0;
+	for (unsigned int i = 0; i < GRIDSIZE / 2; ++i) {
+		r1 = rand() % GRIDSIZE;
+		grid[r1].val = 0;
+		grid[r1].use = 1;
+		tilesleft++;
+	}
 
 	print();
-	goto end;
+	printf("\x1b[22m\x1b[0;%uHTiles Left: %u", 
+		(31 - 12 - intsize(tilesleft)) / 2, 
+		tilesleft
+	);
+	//goto end;
+
+	cursorpos = GRIDSIZE / 2;
+	while (grid[cursorpos].use == 0) {
+		cursorpos++;
+	}
 
 	char c;
 
@@ -331,49 +386,89 @@ int main(void) {
 	while ((c = getchar()) != BREAK) {
 
 		// clear previous cursor
-		printf("\x1b[7m\x1b[%u;%uH%c", (gridpos[cursorpos] / gridx) + 2, (gridpos[cursorpos] % gridx) + 3, digittochar(grid[cursorpos]));
+		printf("\x1b[7m\x1b[%u;%uH%c", (gridpos[cursorpos] / gridx) + 2, (gridpos[cursorpos] % gridx) + 3, digittochar(grid[cursorpos].val));
 
-		switch (c) {
+		mainloopkeys:
+			switch (c) {
 
-			case KEYW:
-				cursorpos -= 9;
-				if (cursorpos > GRIDSIZE) {
-					cursorpos += GRIDSIZE;
-				}
-				break;
-				
-			case KEYS:
-				cursorpos += 9;
-				if (cursorpos > GRIDSIZE) {
-					cursorpos -= GRIDSIZE;
-				}
-				break;
-				
-			case KEYA:
-				if (cursorpos % 9 == 0) {
-					cursorpos += 9;
-				}
-				cursorpos--;
-				break;
-				
-			case KEYD:
-				if (cursorpos % 9 == 9 - 1) {
+				case KEYW:
 					cursorpos -= 9;
-				}
-				cursorpos++;
-				break;
+					if (cursorpos > GRIDSIZE) {
+						cursorpos += GRIDSIZE;
+					}
+					break;
+					
+				case KEYS:
+					cursorpos += 9;
+					if (cursorpos > GRIDSIZE) {
+						cursorpos -= GRIDSIZE;
+					}
+					break;
+					
+				case KEYA:
+					if (cursorpos % 9 == 0) {
+						cursorpos += 9;
+					}
+					cursorpos--;
+					break;
+					
+				case KEYD:
+					if (cursorpos % 9 == 9 - 1) {
+						cursorpos -= 9;
+					}
+					cursorpos++;
+					break;
+
+				case KEY0:
+					if (grid[cursorpos].val == 0) break;
+					grid[cursorpos].val = 0;
+					tilesleft++;
+					printf("\x1b[0;%uHTiles Left: %u", 
+						(31 - 12 - intsize(tilesleft)) / 2, 
+						tilesleft
+					);
+					break;
+
+				default:
+
+					if (c > KEY0 && c <= KEY0 + 9) {
+
+						temp = grid[cursorpos].val;
+						grid[cursorpos].val = c - KEY0;	
+						if (!checkgrid()) {
+							grid[cursorpos].val = temp;
+						} else if (temp == 0) {
+							tilesleft--;
+							if (tilesleft == 0) goto win;
+							printf("\x1b[0;%uHTiles Left: %u", 
+								(31 - 12 - intsize(tilesleft)) / 2, 
+								tilesleft
+							);
+						}
+					
+					}
+
+			}
+
+		if (grid[cursorpos].use == 0) {
+			goto mainloopkeys;
 		}
 
 		mainloopcontinue:
 
-			printf("\x1b[27m\x1b[%u;%uH%c", (gridpos[cursorpos] / gridx) + 2, (gridpos[cursorpos] % gridx) + 3, digittochar(grid[cursorpos]));
+			printf("\x1b[27m\x1b[%u;%uH%c", (gridpos[cursorpos] / gridx) + 2, (gridpos[cursorpos] % gridx) + 3, digittochar(grid[cursorpos].val));
 		
 	}
 
 	end:
 
 		tcsetattr(0, TCSANOW, &restore); // restore terminal state
-		printf("\x1b[25h\x1b[0m\x1b[%u;0HBYE BYE\n", gridy + 2 + 1 + 60); // TODO - 1 - 100
+		printf("\x1b[25h\x1b[0m\x1b[%u;0\n", gridy + 2 + 1 + 60); // TODO - 1 - 100
 
 		return 0;
+
+	win:
+		printf("\x1b[0;12HYou Win");
+		goto end;
+		
 }
